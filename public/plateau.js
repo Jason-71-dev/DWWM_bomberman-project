f399e0a6b7cd79252bb93c75177fadc766db2021
Jason VILLIN <jvillin@irfabourgogne.local>
2025-06-18 13:51:21 +0200

    test

--- public/plateau.js
+++ public/plateau.js
@@ -2,9 +2,14 @@ const playground = document.querySelector(".playground")
 const cols = 33
 const rows = 33
 const cellSize = 24
+const grid = []
 
 for (let y = 0; y < rows; y++) {
+    const row = []
     for (let x = 0; x < cols; x++) {
+        let cellType;
+        if (x === 0 || x === cols - 1 || y === 0 || y === rows - 1) {
+            
         const cell = document.createElement("div");
         cell.classList.add("cell");
         playground.appendChild(cell);
@@ -16,14 +21,16 @@ for (let y = 0; y < rows; y++) {
             // Bords extérieurs
             cell.classList.add("wall");
         }
-        else if (x % 2 === 0 && y % 2 ===0) {
-            cell.classList.add("wall")
+        else if (Math.random() < 0.1) {
+            cell.classList.add("wall");
         }
         else if (Math.random() < 0.2) {
             // 20% de chance -> bloc destructible
+            cell.classList.add("destructible");
         }
         else {
-            cell.classList.add("empty")
+            cell.classList.add("empty"); // sol vide
         }
     }
+
 }
