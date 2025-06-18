6d3da12f94de1e977d76aeae95d551bb8a465833
Jason <jason-71@outlook.fr>
2025-06-18 21:24:26 +0200

    Update

--- public/plateau.js
+++ public/plateau.js
@@ -4,33 +4,38 @@ const rows = 33
 const cellSize = 24
 const grid = []
 
+
 for (let y = 0; y < rows; y++) {
     const row = []
     for (let x = 0; x < cols; x++) {
-        let cellType;
-        if (x === 0 || x === cols - 1 || y === 0 || y === rows - 1) {
-            
         const cell = document.createElement("div");
         cell.classList.add("cell");
-        playground.appendChild(cell);
         cell.style.left = `${x * cellSize}px`;
         cell.style.top = `${y * cellSize}px`;
 
-        // TYPES DE BLOCS
-        if (x === 0 || x === cols - 1 || y === 0 || y === rows - 1) {
-            // Bords extérieurs
-            cell.classList.add("wall");
+        // Déterminer le type
+        let cellType;
+        const isSpawnZone = (x <= 2 && y <= 2);
+        const isEdge = (x === 0 || x === cols - 1 || y === 0 || y === rows - 1);
+        
+        if (isEdge) {
+            cellType = 'borderWall';
+        } else if (isSpawnZone) {
+            cellType = 'empty';
+        } else {
+            const r = Math.random();
+            if (r < 0.1) {
+                cellType = 'fixedWall';
+            }
+            else if (r < 0.3) {
+                cellType = 'destructible';
+            } 
+            else {
+                cellType = 'empty';
+            }
         }
-        else if (Math.random() < 0.1) {
-            cell.classList.add("wall");
-        }
-        else if (Math.random() < 0.2) {
-            // 20% de chance -> bloc destructible
-            cell.classList.add("destructible");
-        }
-        else {
-            cell.classList.add("empty"); // sol vide
-        }
-    }
-
-}
+        
+        cell.classList.add(cellType); // affecte le visuel
+        row.push(cellType);           // ajoute à la mémoire
+        playground.appendChild(cell); // ajoute à l'écran
+}}
