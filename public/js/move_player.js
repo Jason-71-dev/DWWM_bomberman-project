9268651f21c88d09233b76f98e4d1bcefd11c2c5
Jason <jason-71@outlook.fr>
2025-06-19 15:13:57 +0200

    fix joueur indéplacable

--- public/js/move_player.js
+++ public/js/move_player.js
@@ -1,32 +1,35 @@
 const player = document.querySelector(".player");
 player.style.width = "24px";
 player.style.height = "24px";
-player.style.left = `${cellSize}px`;  // position x=1
-player.style.top = `${cellSize}px`;   // position y=1
+player.style.left = `${cellSize}px`;
+player.style.top = `${cellSize}px`;
 
-let playerPosition = {x: 0, y: 0 };
+let playerPosition = { x: 0, y: 0 };
+const maxX = 30;
+const maxY = 30;
 
 document.addEventListener("keydown", movePlayer);
 
 function movePlayer(event) {
     switch(event.key) {
-        case "ArrowUp" :
-            playerPosition.y = Math.max(0, playerPosition.y - 1)
+        case "ArrowUp":
+            playerPosition.y = Math.max(0, playerPosition.y - 1);
             break;
-        case "ArrowDown" :
-            playerPosition.y = Math.min(0, playerPosition.y + 1)
+        case "ArrowDown":
+            playerPosition.y = Math.min(maxY, playerPosition.y + 1);
             break;
-        case "ArrowLeft" : 
-            playerPosition.x = Math.max(0, playerPosition.x -1)
+        case "ArrowLeft":
+            playerPosition.x = Math.max(0, playerPosition.x - 1);
             break;
-        case "ArrowRight" :
-            playerPosition.x = Math.min(0, playerPosition.x + 1)
-            break;             
+        case "ArrowRight":
+            playerPosition.x = Math.min(maxX, playerPosition.x + 1);
+            break;
     }
     updatePlayerPosition();
 }
+
 function updatePlayerPosition() {
-    const player = document.getElementById("player");
-    player.style.gridRowStart = playerPosition.x + 1;
-    player.style.gridColumnStart = playerPosition.y + 1;
+    const player = document.querySelector(".player");
+    player.style.gridRowStart = playerPosition.y + 1;
+    player.style.gridColumnStart = playerPosition.x + 1;
 }
